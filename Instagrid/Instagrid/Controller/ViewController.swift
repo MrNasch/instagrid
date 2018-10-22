b3eaafc36cd44f3928446e89d549b43fc7227219
MrNasch <nicolaspaschal@hotmil.com>
2018-10-22 22:18:00 +0200

    initial commit

---
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Paschal on 27/09/18.
+//  Copyright © 2018 nasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var squareStackView: UIStackView!
+    @IBOutlet weak var topStackView: UIStackView!
+    @IBOutlet weak var bottomStackView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func layout1x2(_ sender: Any) {
+        
+    }
+    @IBAction func layout2x1(_ sender: Any) {
+        
+    }
+    @IBAction func layoutLeft1x2(_ sender: Any) {
+        
+    }
+    @IBAction func layout2x2(_ sender: Any) {
+        
+    }
+}
+
