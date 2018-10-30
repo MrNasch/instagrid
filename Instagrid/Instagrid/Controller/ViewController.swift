9bd50c41ca00d5c6567dd0ac4ea746a459e0a361
MrNasch <nicolaspaschal@hotmil.com>
2018-10-30 18:21:39 +0100

    modified view in landscape

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    func makeButton() -> UIButton {
+        let button = UIButton()
+        button.frame = CGRect(x: self.view.frame.size.width - 60, y: 60, width: 50, height: 50)
+        button.backgroundColor = UIColor.red
+        button.setTitle("", for: .normal)
+        button.addTarget(self, action: #selector(resetLayout), for: .touchUpInside)
+        self.view.addSubview(button)
+        return button
+    }
     
     @IBOutlet weak var squareStackView: UIStackView!
     @IBOutlet weak var topStackView: UIStackView!
@@ -17,7 +26,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    func resetLayout() {
+    @objc func resetLayout() {
         for view in topStackView.subviews {
             view.removeFromSuperview()
         }
@@ -26,31 +35,30 @@ class ViewController: UIViewController {
             view.removeFromSuperview()
         }
     }
-    
     @IBAction func layout1x2(_ sender: Any) {
         resetLayout()
-        self.topStackView.addArrangedSubview(topStackView)
-        self.bottomStackView.addArrangedSubview(bottomStackView)
-        self.bottomStackView.addArrangedSubview(bottomStackView)
+        topStackView.addArrangedSubview(topStackView)
+        bottomStackView.addArrangedSubview(bottomStackView)
+        bottomStackView.addArrangedSubview(bottomStackView)
     }
     @IBAction func layout2x1(_ sender: Any) {
         resetLayout()
-        self.topStackView.addArrangedSubview(topStackView)
-        self.topStackView.addArrangedSubview(topStackView)
-        self.bottomStackView.addArrangedSubview(bottomStackView)
+        topStackView.addArrangedSubview(topStackView)
+        topStackView.addArrangedSubview(topStackView)
+        bottomStackView.addArrangedSubview(bottomStackView)
     }
     @IBAction func layoutLeft1x2(_ sender: Any) {
         resetLayout()
-        self.topStackView.addArrangedSubview(topStackView)
-        self.bottomStackView.addArrangedSubview(bottomStackView)
-        self.bottomStackView.addArrangedSubview(bottomStackView)
+        topStackView.addArrangedSubview(topStackView)
+        bottomStackView.addArrangedSubview(bottomStackView)
+        bottomStackView.addArrangedSubview(bottomStackView)
     }
     @IBAction func layout2x2(_ sender: Any) {
         resetLayout()
-        self.topStackView.addArrangedSubview(topStackView)
-        self.topStackView.addArrangedSubview(topStackView)
-        self.bottomStackView.addArrangedSubview(bottomStackView)
-        self.bottomStackView.addArrangedSubview(bottomStackView)
+        topStackView.addArrangedSubview(topStackView)
+        topStackView.addArrangedSubview(topStackView)
+        bottomStackView.addArrangedSubview(bottomStackView)
+        bottomStackView.addArrangedSubview(bottomStackView)
     }
 }
 
