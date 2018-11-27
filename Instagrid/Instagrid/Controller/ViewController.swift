db42323cee932c26becff3f044d133edc413c3bf
MrNasch <nicolaspaschal@hotmil.com>
2018-11-27 04:29:34 +0100

    created imageïcker func

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func makeButton(images: UIImage) -> UIButton {
         let button = UIButton()
         button.backgroundColor = UIColor.white
         button.setImage(images, for: .normal)
+        button.addTarget(self, action: #selector(self.chooseImage(_:)), for: .touchUpInside)
         return button
     }
     
@@ -25,6 +26,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         layout2x1(self)
     }
+    @objc func chooseImage(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                print("Camera not available")
+            }
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let imageChoose = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        for _ in layoutManager.images {
+            var i = 0
+            layoutManager.images[i] = imageChoose
+            makeButton(images: imageChoose).setBackgroundImage(imageChoose, for: .normal)
+            makeButton(images: imageChoose).contentMode = .scaleAspectFit
+            i += 1
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
     func makeGridLayout() {
         resetLayout()
         let firstLine = layoutManager.imagesGrid[0]
